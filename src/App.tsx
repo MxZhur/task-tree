f5255c8562d4e956b45269dbf6c3f38313eef7e4
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-02-28 14:19:27 +0700

    Added React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MainPage } from "./pages";
 import Layout from "./components/Layout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (

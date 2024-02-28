afefee9439b6d36e282b38e89c0fb98603c3298a
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-02-28 18:20:48 +0700

    Implemented task selection and tree collapsing

--- src/components/TaskTree/TaskTreeItem.tsx
+++ src/components/TaskTree/TaskTreeItem.tsx
@@ -3,15 +3,25 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { Task } from "../../store/tasksSlice";
 import "./TaskTreeItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faChevronDown, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faChevronDown,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { ProgressBar } from "react-bootstrap";
 import { setSelectedTask } from "../../store/selectedTaskSlice";
 
 interface TaskTreeItemProps {
   task: Task;
+  collapsedItems?: string[];
+  toggleItemExpansion: (taskId: string) => void;
 }
 
-const TaskTreeItem: React.FC<TaskTreeItemProps> = ({ task }) => {
+const TaskTreeItem: React.FC<TaskTreeItemProps> = ({
+  task,
+  collapsedItems,
+  toggleItemExpansion
+}) => {
   const dispatch = useAppDispatch();
 
   const childTasks = useAppSelector((state) =>
@@ -20,28 +30,41 @@ const TaskTreeItem: React.FC<TaskTreeItemProps> = ({ task }) => {
       .filter((e) => e !== undefined)
   );
 
-  const isSelected = useAppSelector((state) => state.selectedTask.taskId === task.id);
-
-  const isExpanded = true;
-
-  const expandButton = <FontAwesomeIcon icon={isExpanded ? faChevronDown : faChevronRight} />;
-  const expandButtonPlaceHolder = (
-    <div style={{ height: "1rem", width: "10px" }} />
+  const isSelected = useAppSelector(
+    (state) => state.selectedTask.taskId === task.id
   );
 
   const handleClick = () => {
     dispatch(setSelectedTask(task.id));
-  }
-  
+  };
+
   const handleDoubleClick = () => {
     // TODO: Go to edit form
+  };
+
+  const handleArrowClick = () => {
+    toggleItemExpansion(task.id);
   }
+
+  const isExpanded =
+    collapsedItems !== undefined && !collapsedItems?.includes(task.id);
+
+  const expandButton = (
+    <div className="task-tree-item-arrow-container">
+      <FontAwesomeIcon icon={isExpanded ? faChevronDown : faChevronRight} onClick={handleArrowClick} />
+    </div>
+
+  );
+  const expandButtonPlaceHolder = (
+    <div className="task-tree-item-arrow-container" />
+  );
 
   return (
     <>
       <div
         className={
-          "task-tree-item-container" + (isSelected ? " task-tree-item-active" : "")
+          "task-tree-item-container" +
+          (isSelected ? " task-tree-item-active" : "")
         }
         onClick={handleClick}
         onDoubleClick={handleDoubleClick}
@@ -49,9 +72,7 @@ const TaskTreeItem: React.FC<TaskTreeItemProps> = ({ task }) => {
         <div className="task-tree-item-title unselectable">
           {childTasks.length > 0 ? expandButton : expandButtonPlaceHolder}
           <div style={{ width: "5px" }}></div>
-          <div className="task-tree-item-name">
-            {task.name}
-          </div>
+          <div className="task-tree-item-name">{task.name}</div>
         </div>
         {task!.progress! < 100 ? (
           <ProgressBar
@@ -64,14 +85,22 @@ const TaskTreeItem: React.FC<TaskTreeItemProps> = ({ task }) => {
           />
         ) : (
           <div className="task-tree-item-checkmark-container">
-            <FontAwesomeIcon icon={faCheck} color={isSelected ? "white" : "green"} />
+            <FontAwesomeIcon
+              icon={faCheck}
+              color={isSelected ? "white" : "green"}
+            />
           </div>
         )}
       </div>
-      {childTasks.length > 0 && (
+      {(isExpanded && childTasks.length > 0) && (
         <div className="task-tree-child-container">
           {childTasks.map((t) => (
-            <TaskTreeItem key={t?.id} task={t!} />
+            <TaskTreeItem
+              key={t?.id}
+              task={t!}
+              collapsedItems={collapsedItems}
+              toggleItemExpansion={toggleItemExpansion}
+            />
           ))}
         </div>
       )}
--- src/components/TaskTree/index.tsx
+++ src/components/TaskTree/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppSelector } from "../../store/hooks";
 import { TaskTreeItem } from "./TaskTreeItem";
 import "./index.css";
@@ -10,10 +10,29 @@ const TaskTree: React.FC = () => {
       .filter((e) => e !== null);
   });
 
+  const [collapsedItems, setCollapsedItems] = useState<string[]>([]);
+
+  const toggleItemExpansion = (taskId: string) => {
+    let newCollapsedItems = [...collapsedItems];
+
+    if (collapsedItems.includes(taskId)) {
+      newCollapsedItems = newCollapsedItems.filter(e => e !== taskId);
+    } else {
+      newCollapsedItems.push(taskId);
+    }
+
+    setCollapsedItems(newCollapsedItems);
+  }
+
   return (
     <div className="task-tree-container">
       {tasks.map((t) => (
-        <TaskTreeItem key={t?.id} task={t!} />
+        <TaskTreeItem
+          key={t?.id}
+          task={t!}
+          collapsedItems={collapsedItems}
+          toggleItemExpansion={toggleItemExpansion}
+        />
       ))}
     </div>
   );

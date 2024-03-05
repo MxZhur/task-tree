93911dc4ea7364de78a41bfc64f396a1958a9d2a
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-03-05 13:41:45 +0700

    Bumped the version number

--- src/utils/appInfo.ts
+++ src/utils/appInfo.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "Task Tree";
-export const APP_VERSION = "0.0.1";
+export const APP_VERSION = "0.0.2";
 export const APP_RELEASE_YEAR = "2024";
 export const APP_AUTHOR = "Maxim Zhuravlev";
